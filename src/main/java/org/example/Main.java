8422777061c868cf9db553d6faa362e65d2f24c4
CristianCanoIng <canox09@gmail.com>
2025-02-21 10:19:21 -0500

    Ejemplo Switch Case Viejo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,30 @@
 package org.example;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Solo");
+
+        System.out.println("Switch Case Viejo Ejemplo\n");
+
+        //Escoja una opcion
+        System.out.println("Escoja: 1 2 3");
+        Scanner teclado = new Scanner(System.in);
+        int numeroIngresado = teclado.nextInt();
+
+        switch (numeroIngresado) {
+            case 1:
+                System.out.println("Opcion 1");
+                break;
+            case 2:
+                System.out.println("Opcion 2");
+                break;
+            case 3:
+                System.out.println("Opcion 3");
+                break;
+            default:
+                System.out.println("No esta dentro de las opciones");
+                break;
+        }
     }
 }
