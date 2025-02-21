1897721dd368a48e8e123addae958155781250d8
CristianCanoIng <canox09@gmail.com>
2025-02-21 10:30:29 -0500

    Ejemplo Switch Case Nuevo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,26 +5,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Switch Case Viejo Ejemplo\n");
+        System.out.println("Switch Case Nuevo Ejemplo\n");
 
         //Escoja una opcion
         System.out.println("Escoja: 1 2 3");
         Scanner teclado = new Scanner(System.in);
         int numeroIngresado = teclado.nextInt();
 
-        switch (numeroIngresado) {
-            case 1:
-                System.out.println("Opcion 1");
-                break;
-            case 2:
-                System.out.println("Opcion 2");
-                break;
-            case 3:
-                System.out.println("Opcion 3");
-                break;
-            default:
-                System.out.println("No esta dentro de las opciones");
-                break;
-        }
+        String mensaje = switch (numeroIngresado) {
+            case 1 -> "La opcion es uno";
+            case 2 -> "La opcion es dos";
+            case 3 -> "La opcion es tres";
+            default -> "No esta en las opciones";
+        };
+        System.out.println(mensaje);
+
+        //Otra forma
+        /*String tipoOpcion = switch (numeroIngresado) {
+            case 1, 2, 3 -> " Opcion en lista";
+            default -> "No esta esta opcion";
+        };*/
     }
 }
